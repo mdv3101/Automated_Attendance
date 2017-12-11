176b49f222e2067c0e8e4554ff968300edc7f5d5
Madhav Agarwal <madhavagarwal99@gmail.com>
2017-12-11 19:18:29 +0530

    Add files via upload

--- add_user.py
+++ add_user.py
@@ -1,7 +1,16 @@
+'''
+This file is use for training purpose.
+In it, the user will going to enter his Name & Student No.
+After it, it will capture 20images of the user.
+Then the name and student no. is going to save in a seperate csv file
+
+'''
+
 import numpy
 import cv2
 import os
 import dlib
+import csv
 
 Dept_Map={10 : "CSE" ,13 :"IT",31: "EC", 00: "CE", 21: "EN", 32: "EI",43: "ME",
           70: "MBA", 14:"MCA"} 
@@ -46,10 +55,33 @@ while(True):
     cv2.imshow('frame',img)
     cv2.waitKey(1)
     if(Num_img >=20):
-        break
-    
+        break    
 cap.release()
 cv2.destroyAllWindows()
+
+path_database = os.path.join(os.getcwd(),"database/")
+file_name_csv= path+"stud_info_"+Dept+"_"+Batch+".csv"
+
+if not os.path.exists(file_name_csv):
+    with open(file_name_csv,'w',newline='') as csvfile:
+        filewriter = csv.writer(csvfile, delimiter=',') 
+        filewriter.writerows([['Student_No','Name'],
+                            [stud_no,stud_name]])
+        
+        csvfile.close()
+flag=0  
+with open(file_name_csv) as f:
+    csv_file= csv.reader(f,delimiter=',')
+    for row in csv_file:
+        if stud_no==row[0]:
+            flag=1
+f.close()
+
+if flag==0:
+     with open(file_name_csv,'a',newline='') as csvfile:
+        filewriter = csv.writer(csvfile, delimiter=',') 
+        filewriter.writerow([stud_no,stud_name])
+        csvfile.close()
         
         
         
